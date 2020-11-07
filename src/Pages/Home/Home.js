50c6744134fffcf82318c1c6f1f4e715d3d83dea
valentinEutrope <valentin.eutrope@free.fr>
2020-11-07 18:49:16 +0100

    feat(components + pages): implement base component to test the theme

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { ButtonRounded } from "../../components/base/Buttons";
+
 const Home = () => {
   return (
     <div className="App">
       <h1>SMITH</h1>
-      <Link to="/login">se déconnecter</Link>
+      <Link to="/login">
+        <ButtonRounded>Se déconnecter</ButtonRounded>
+      </Link>
     </div>
   );
 };
