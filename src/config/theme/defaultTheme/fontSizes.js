60065583a76b57087f5d5ef11e24345336ce36f7
valentinEutrope <valentin.eutrope@free.fr>
2020-11-07 18:48:33 +0100

    feat(config: theme): update themeHelpers

--- src/config/theme/defaultTheme/fontSizes.js
+++ src/config/theme/defaultTheme/fontSizes.js
@@ -3,6 +3,7 @@ import { baseFontSize } from "./variables";
 export const fontSizes = {
   base: baseFontSize,
   paragraph: baseFontSize * 1.125,
+  button: baseFontSize * 1.25,
   header: {
     big: baseFontSize * 2.5,
     medium: baseFontSize * 2,
--- src/config/theme/themeHelpers.js
+++ src/config/theme/themeHelpers.js
@@ -14,15 +14,15 @@ export const breakpoint = (breakpoint) => ({ theme }) =>
   theme.breakpoints[breakpoint];
 
 export const th = (key, unit = "rem") => ({ theme }) => {
-  var data = _get(theme, key);
+  let data = _get(theme, key);
 
   if (key && (key.includes("spaces") || key.includes("sizes"))) {
     data = String(data).concat(unit);
   }
 
   if (key && key.includes("colors")) {
-    var hasLevelColor = key.split(".").length > 2;
-    var defaultLevel = "500";
+    let hasLevelColor = key.split(".").length > 2;
+    let defaultLevel = "500";
 
     data = _get(theme, hasLevelColor ? key : `${key}.${defaultLevel}`);
   }
