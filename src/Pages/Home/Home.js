7fe0463208eed347b408c095c82a8607a188f5a6
Eutrope Valentin <valentin.eutrope@free.fr>
2020-12-10 01:25:26 +0100

    feat(src): implement a exemple styled button

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { ButtonRounded } from "../../components/base/Buttons";
+import { ButtonDanger } from "../../components/base/Buttons";
 
 const Home = () => {
   return (
     <div className="App">
       <h1>SMITH</h1>
       <Link to="/login">
-        <ButtonRounded>Se déconnecter</ButtonRounded>
+        <ButtonDanger radius="1rem">Se déconnecter</ButtonDanger>
       </Link>
     </div>
   );
--- src/components/base/Buttons.js
+++ src/components/base/Buttons.js
@@ -7,10 +7,32 @@ export const Button = styled.button`
   font-size: 1.25rem;
   cursor: pointer;
 
+  border: 0;
+
   background-color: ${th("colors.white")};
   color: ${th("colors.black")};
 `;
 
 export const ButtonRounded = styled(Button)`
   border-radius: 6rem;
+`;
+
+export const ButtonSuccess = styled(Button)`
+  background-color: ${th("colors.success")};
+  border-radius: ${(props) => props.radius || "0"};
+  transition: background-color 300ms;
+
+  &:hover {
+    background-color: ${th("colors.success.700")};
+  }
+`;
+
+export const ButtonDanger = styled(Button)`
+  background-color: ${th("colors.danger")};
+  border-radius: ${(props) => props.radius || "0"};
+  transition: background-color 300ms;
+
+  &:hover {
+    background-color: ${th("colors.danger.700")};
+  }
 `;
