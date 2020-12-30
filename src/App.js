e097f2c48fc928824dd1d29c1e4e7d576cd270d6
valentinEutrope <valentin.eutrope@free.fr>
2020-12-30 18:49:20 +0100

    feat(src): add template route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Providers from "./Providers";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
+import CharacterSheetTemplate from "./Pages/SheetTemplates/CharacterSheetTemplate";
 
 const App = () => {
   return (
@@ -15,6 +16,9 @@ const App = () => {
           </Route>
           <Route exact path="/login">
             <Login />
+          </Route>
+          <Route exact path="/template/character-sheet">
+            <CharacterSheetTemplate />
           </Route>
         </Switch>
       </Router>
