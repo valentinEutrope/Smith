670f5b2d5641740a7938b133ec0fa6816b87a6bd
cabbadie <cabbadie.pro@gmail.com>
2021-10-06 23:26:15 +0200

    cleanup

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -4,9 +4,7 @@ import GlobalStyle from "./config/theme/defaultTheme/GlobalStyle";
 
 import Providers from "./Providers";
 import Home from "./Pages/Home";
-import Login from "./Pages/Login";
 import CharacterSheetTemplate from "./Pages/SheetTemplates/CharacterSheetTemplate";
-import Back from "./Pages/Back";
 
 const App = () => {
   return (
@@ -18,14 +16,8 @@ const App = () => {
             <Route exact path="/">
               <Home />
             </Route>
-            <Route exact path="/login">
-              <Login />
-            </Route>
             <Route exact path="/template/character-sheet">
               <CharacterSheetTemplate />
-            </Route>
-            <Route exact path="/back">
-              <Back />
             </Route>
           </Switch>
         </Router>
