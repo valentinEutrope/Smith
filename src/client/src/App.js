c42fb3c2c5564b87b1add87a7470b13c5f40171c
Eutrope Valentin <valentin.eutrope@free.fr>
2021-10-15 19:06:16 +0200

    Create primary button and add create new world button

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -5,7 +5,8 @@ import GlobalStyle from "./config/theme/defaultTheme/GlobalStyle";
 import Providers from "./Providers";
 import Home from "./Pages/Home";
 import CharacterSheetTemplate from "./Pages/SheetTemplates/CharacterSheetTemplate";
-import World from "./Pages/World/World";
+import World from "./Pages/World";
+import NewWorld from "./Pages/NewWorld";
 
 const App = () => {
   return (
@@ -16,6 +17,9 @@ const App = () => {
           <Switch>
             <Route exact path="/">
               <Home />
+            </Route>
+            <Route exact path="/new-world">
+              <NewWorld />
             </Route>
             <Route exact path="/template/character-sheet">
               <CharacterSheetTemplate />
