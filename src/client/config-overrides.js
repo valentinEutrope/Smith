e28857095dc9506199e22173f292047c567d776a
Eutrope Valentin <valentin.eutrope@free.fr>
2021-12-13 19:26:10 +0100

    Implement react-app-rewire-alias

---
+++ src/client/config-overrides.js
@@ -0,0 +1,15 @@
+const { alias } = require("react-app-rewire-alias");
+
+module.exports = {
+  webpack: (config, env) => {
+    alias({
+      "@components": "./src/components",
+      "@base": "./src/components/base",
+      "@assets": "./src/assets",
+      "@config": "./src/config",
+      "@pages": "./src/Pages",
+    })(config);
+
+    return config;
+  },
+};
